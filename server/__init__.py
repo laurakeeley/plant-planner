62e5a0fd2acee1426bb08501510d516b78455037
Your GitHub Username <joyhe520@gmail.com>
2023-08-18 15:03:24 -0500

    sign up route working

--- server/__init__.py
+++ server/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 
+#*initialze application
 def create_app():
     app = Flask(__name__)
+    bcrypt = Bcrypt(app)
     app.config['SECRET_KEY'] = 'sksjdksdjklsdjsldfeetewsvnh'
     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://Joy:password@localhost:5432/plantplanner"
     db.init_app(app)
     
-    # from .api import api as api_blueprint
-    # app.register_blueprint(api_blueprint, url_prefix='/api/')
+    from .api.auth import auth
+    app.register_blueprint(auth, url_prefix='/')
     
     
     #*load the models from db
@@ -23,5 +27,14 @@ def create_app():
         except Exception as e:
             print(f"An error occurred while creating the database: {e}")
     
+    #!this is to create the gatekeeper
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        #*looking for primary key int(id)
+        return User.query.get(int(id))
     
     return app
---
+++ server/api/auth.py
@@ -0,0 +1,47 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify, Flask
+from ..models import User
+# from werkzeug.security import generate_password_hash, check_password_hash
+from flask_bcrypt import Bcrypt, generate_password_hash, check_password_hash
+
+from .. import db
+from flask_login import login_user, login_required, logout_user, current_user
+
+
+#! initialize blueprint, always need to pass in __name__, and the name of the blueprint
+auth = Blueprint("auth", __name__)
+
+
+@auth.route('/sign-up', methods=['GET', "POST"])
+def sign_up():
+    if request.method == "POST":
+        data = request.json
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        email = data.get('email')
+        password = data.get('password')
+        
+        response = {}
+        
+        #*check to see if the email has been registered
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("Email has been registered", category='error')
+            response = {'message': 'Email has been registered', 'status': 404}
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character', category='error')
+        elif len(password) < 4:
+            flash('Password must be at least 4 characters', category='error')
+        else:
+            new_user = User(first_name=first_name, last_name= last_name, email=email, password=generate_password_hash(password).decode('utf-8'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            response = {'message': 'User registered successfully', 'status': 200}
+            print(f"jsonify: {jsonify(response)}")
+        return jsonify(response)
+        
+            
+    return jsonify({'message': 'Invalid request', 'status': 400})
