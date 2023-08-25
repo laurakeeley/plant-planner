a7a1a47e76d416be224f0ffef920d814ed2ffc76
laurakeeley <l.keeley000@gmail.com>
2023-08-25 14:07:06 -0500

    adds login to auth and login ts, creates responsive login inputs

--- plant-planner-client/src/app/login/login.component.ts
+++ plant-planner-client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +8,23 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   loginImage: string;
+  email = "";
+  password = "";
 
-  constructor() {
+  constructor(private auth:AuthService) {
     this.loginImage = "assets/pexelssamuelcrosland2557232-1@2x.png"
+  }
+
+  login() {
+    this.auth.login(this.email, this.password).subscribe(
+      (response) => {
+        console.log(response);
+        // Handle successful login, e.g., navigate to a different page
+      },
+      (error) => {
+        console.error(error);
+        // Handle login error
+      }
+    )
   }
 }
