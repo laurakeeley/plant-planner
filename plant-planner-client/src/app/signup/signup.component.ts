c768670b5a87cd76ba5e12f96e6e68072a6d853e
Your GitHub Username <joyhe520@gmail.com>
2023-08-25 11:52:15 -0500

    fixed cors

--- plant-planner-client/src/app/signup/signup.component.ts
+++ plant-planner-client/src/app/signup/signup.component.ts
@@ -10,6 +10,8 @@ import { AuthService } from '../services/auth.service';
 })
 export class SignupComponent { 
   newUser = {
+    first_name: "test",
+    last_name: "test",
     email: "test@test.com",
     password: "test123"
   };
