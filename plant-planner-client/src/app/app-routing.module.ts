d752a8d10769a40949b2889725b008f5ef60f0f3
laurakeeley <l.keeley000@gmail.com>
2023-09-01 15:09:09 -0500

    adds search route

--- plant-planner-client/src/app/app-routing.module.ts
+++ plant-planner-client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Router, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'search', component: SearchComponent},
 ];
 
 @NgModule({
