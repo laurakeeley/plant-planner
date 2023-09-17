1e2269842ec0e0c0385fb1b9745c9085e076af66
Your GitHub Username <joyhe520@gmail.com>
2023-09-17 14:01:15 -0500

    card component

--- plant-planner-client/src/app/search/search.component.ts
+++ plant-planner-client/src/app/search/search.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { SearchDataService } from '../services/search-data.service';
 import { AuthService } from '../services/auth.service';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HARDINESS_ZONE_KEY } from '../env';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -10,21 +11,23 @@ import { AuthService } from '../services/auth.service';
 export class SearchComponent {
   searchResults: any = [];
   auth: AuthService;
+  zipCodeResults: any = []
 
   constructor (
     private searchData:SearchDataService,
     private authService:AuthService
+    
   ) {
     this.auth = authService;
   }
 
   ngOnInit() {
-    this.getSearchData();
+    // this.getSearchData();
   }
 
   //banner words
   words: string[] = ['Easy care', 'Help native wildlife', 'Best plants for you'];
-
+  query: string = ''
   //filter section
   sunlight = {
     full_shade: false,
@@ -35,6 +38,43 @@ export class SearchComponent {
   indoor = false;
   hardinessZone = '';
 
+  //fetch hardiness zone based on zip code
+  getHardinessZone(){
+    console.log(this.hardinessZone)
+    
+    this.searchData.getHardinessZoneData(this.hardinessZone).subscribe(
+      (result: string) => {
+        console.log(result);
+      },
+      (error: any) => {
+        console.error(error);
+      })
+  }
+
+
+  
+  saveCheckboxValues(){
+  
+    if(this.sunlight.full_shade){
+      this.query += '&sunlight=full_sun'
+    }
+    if(this.sunlight.part_shade){
+      this.query += '&sunlight=part_sun'
+    }
+    if(this.sunlight.sun_part_shade){
+      this.query += '&sunlight=sun-part_shade'
+    }
+    if(this.sunlight.full_sun){
+      this.query += '&sunlight=full_sun'
+    }
+    if(this.indoor){
+      this.query += '&indoor=1'
+    }
+    this.getHardinessZone()
+    console.log(this.query)
+  }
+  
+  //toggle each filter section to expand 
   showSunlight = false;
   showIndoor = false;
   showHardinessZone = false;
@@ -50,15 +90,15 @@ export class SearchComponent {
     this.showHardinessZone = !this.showHardinessZone;
   }
 
-  getSearchData() {
-    this.searchData.getSearchData().subscribe({
-      next: response => {
-        console.log(response.data);
-        this.searchResults = response.data;
-      },
-      error: error => {
-        console.log(error);
-      }
-    })
-  }
+  // getSearchData() {
+  //   this.searchData.getSearchData().subscribe({
+  //     next: response => {
+  //       console.log(response.data);
+  //       this.searchResults = response.data;
+  //     },
+  //     error: error => {
+  //       console.log(error);
+  //     }
+  //   })
+  // }
 }
--- plant-planner-client/src/app/services/search-data.service.ts
+++ plant-planner-client/src/app/services/search-data.service.ts
@@ -1,15 +1,30 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { PLANT_SEARCH_KEY } from '../env';
+import { HARDINESS_ZONE_KEY } from '../env';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchDataService {
-
+  private hardinessZoneApiUrl = 'https://plant-hardiness-zone.p.rapidapi.com/zipcodes/';
+  private headers = new HttpHeaders({
+    'X-RapidAPI-Key': HARDINESS_ZONE_KEY,
+    'X-RapidAPI-Host': 'plant-hardiness-zone.p.rapidapi.com'
+  });
   constructor(private http:HttpClient) { }
 
   getSearchData() {
     return this.http.get<any>(`https://perenual.com/api/species-list?key=${PLANT_SEARCH_KEY}`);
+  }
+  //define hardiness zone function call
+  getHardinessZoneData(zipCode: string): Observable<string>{
+    
+    console.log(`${this.hardinessZoneApiUrl}${zipCode}`)
+    let api = `${this.hardinessZoneApiUrl}${zipCode}`
+    // return this.http.get<any>(`https://plant-hardiness-zone.p.rapidapi.com/zipcodes/${zipCode}/?rapidapi-key=${HARDINESS_ZONE_KEY}`)
+    
+    return this.http.get<string>(api, {headers: this.headers});
   }
 }
