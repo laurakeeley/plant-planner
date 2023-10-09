714a7011c3958781e948c337037f9ef219d4fa51
Your GitHub Username <joyhe520@gmail.com>
2023-10-09 15:37:36 -0500

    get route to get plant detail

--- server/api/views.py
+++ server/api/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+from sqlalchemy.exc import SQLAlchemyError
 from ..models import Plant
 from .. import db
 
@@ -22,36 +23,61 @@ def profile():
 def insert_plant_detail_in_db():
     if request.method == "POST":
         try:
-            #use the plant id to fetch api
             data = request.json
-            #get plant_id, plant_name and details 
+            #*get plant_id, plant_name and details 
             plant_id = data.get('plant_id')            
             plant_name = data.get('plant_name')
             details = data.get("details")
             
-            #validate data type
+            #*validate data type
             if not isinstance(plant_id, int):
                 return jsonify({'error': 'plant_id must be an int'}), 400
             if not isinstance(plant_name, str):
                 return jsonify({'error': 'plant_name must be a string'}), 400
             if not isinstance(details, dict):
                 return jsonify({'error': 'details must be a dictionary'}), 400
-            #check to see if the record is existed
+            
+            #*check to see if the record is existed
             plant_detail_lookup = Plant.query.filter_by(plant_id=plant_id).first()
             if plant_detail_lookup:
                 return jsonify({'error': 'Failed to create a new plant reocrd because the plant exists in db alreay'}), 400
-            #store in the plant db
+            
+            #* if no data exists yet, store in the plant db
             new_plant_detail_record = Plant(plant_id=plant_id, plant_name=plant_name, details=details)
             db.session.add(new_plant_detail_record)
             db.session.commit()
-            #return a response either db insert successfully or err message
+ 
             return jsonify({'message': 'New plant detail record is inserted into Plant db successfully', 'status':200}),200
         except Exception:
             return jsonify({"message": "Failed to insert plant detail into db", "status":400}),400
-@views.route("/searchPlantsDetails", methods=['POST'])
+        
+        
+@views.route("/searchPlantsDetails", methods=['GET'])
 # @token_required
 def search_plants_detail():
-    return jsonify({'message': 'This is a protected route for searching'})
+    if request.method == "GET":
+        try:
+            data = request.json
+            plant_id = data.get("plant_id")
+            if not isinstance(plant_id, int):
+                return jsonify({'error': 'plant_id must be an int'}), 400
+            plant_record = Plant.query.filter_by(plant_id=plant_id).first()
+
+            if plant_record is None:
+                return jsonify({'error': 'Plant record not found'}), 404
+            
+            #* convert SQLAlchemy object into JSON to pass to FE
+            plant_data = {
+                'plant_id': plant_record.plant_id,
+                'plant_name': plant_record.plant_name,
+                'details': plant_record.details
+            }
+            return jsonify({'record': plant_data}), 200
+        
+        except SQLAlchemyError as e:
+            return jsonify({'error': f'Database Error. Failed to get plant record from db because of {str(e)}'}), 500
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
 
 @views.route("/deletePlantsDetails", methods=['DELETE'])
 # @token_required
