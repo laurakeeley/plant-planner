f66dc1fee1524a15258344700e0314c9b8959575
Your GitHub Username <joyhe520@gmail.com>
2023-11-18 13:38:20 -0600

    added script to update db record for capitalized letters

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from server import create_app
 import jwt
 
+from server.api.updatePlantDb import update_plant_names, update_common_name_in_details_field
+
 app = create_app()
 
 if __name__ == '__main__':
